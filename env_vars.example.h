f110bf7c9ae288dfd0b0b3a07664ac7f586ebb35
Anthony Panozzo <panozzaj@gmail.com>
2015-06-26 19:45:30 -0400

    Add env_vars example.

---
+++ env_vars.example.h
@@ -0,0 +1,8 @@
+#ifndef __ENV_VARS_H_
+#define __ENV_VARS_H_
+
+static const char* SERVER_HOSTNAME = "my-coffee-server";
+static const char* BREWING_PATH = "/brewing/123";
+static const char* READY_PATH = "/ready/123";
+
+#endif /* __ENV_VARS_H_ */
