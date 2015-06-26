a76e19826f9c7bb33c521fb9bd146af69a3434a5
Anthony Panozzo <panozzaj@gmail.com>
2015-06-26 19:48:13 -0400

    Finish up initial button press to send message

--- coffee.ino
+++ coffee.ino
@@ -1,21 +1,24 @@
 #include "coffee.h"
 #include "HttpClient.h"
+#include "env_vars.h"
 
 HttpClient http;
 
 // Headers currently need to be set at init, useful for API keys etc.
 http_header_t headers[] = {
-    //  { "Content-Type", "application/json" },
-    //  { "Accept" , "application/json" },
+    { "Content-Type", "application/json" },
+    { "Accept" , "application/json" },
     { "Accept" , "*/*"},
-    { NULL, NULL } // NOTE: Always terminate headers will NULL
+    { NULL, NULL } // NOTE: Always terminate headers with NULL
 };
 
 http_request_t request;
 http_response_t response;
 
-int sent = 0;
+int postedBrewingMessage = 0;
 int buttonPressed = 0;
+int loopDelayInMilliseconds = 100; // in milliseconds
+int iterationsSinceBrewingSent = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -24,26 +27,59 @@ void setup() {
 }
 
 void loop() {
-    delay(500);
+    if (!postedBrewingMessage) {
+        // light up the LED when we have pressed it
+        // but only if we haven't pressed the button recently
+        buttonPressed = digitalRead(D0);
+        digitalWrite(D7, buttonPressed);
 
-    buttonPressed = digitalRead(D0);
-    digitalWrite(D7, buttonPressed);
+        if (buttonPressed) {
+            postedBrewingMessage = 1;
+            iterationsSinceBrewingSent = 0;
 
-    if (!sent && buttonPressed) {
-        sent = 1;
+            Serial.println("posting 'brewing'...");
 
-        Serial.println("sending request...");
+            request.hostname = SERVER_HOSTNAME;
+            request.path = BREWING_PATH;
 
-        request.hostname = "example.com";
-        request.port = 80;
-        request.path = "/";
+            http.post(request, response, headers);
 
-        http.get(request, response, headers);
+            Serial.print("Application>\tResponse status: ");
+            Serial.println(response.status);
 
-        Serial.print("Application>\tResponse status: ");
-        Serial.println(response.status);
-
-        Serial.print("Application>\tHTTP Response Body: ");
-        Serial.println(response.body);
+            Serial.print("Application>\tHTTP Response Body: ");
+            Serial.println(response.body);
+        }
     }
+
+    if (postedBrewingMessage) {
+        iterationsSinceBrewingSent++;
+
+        // flash to say that we got your message
+        digitalWrite(D7, iterationsSinceBrewingSent % 4 == 0);
+
+        // takes ten minutes to brew coffee, send ready message then
+        if (iterationsSinceBrewingSent >= 1000 * 60 * 10 / loopDelayInMilliseconds) {
+            Serial.println("posting 'ready'...");
+
+            request.hostname = SERVER_HOSTNAME;
+            request.path = READY_PATH;
+
+            http.post(request, response, headers);
+
+            Serial.print("Application>\tResponse status: ");
+            Serial.println(response.status);
+
+            Serial.print("Application>\tHTTP Response Body: ");
+            Serial.println(response.body);
+
+            // reset the loop so we can send again
+            postedBrewingMessage = 0;
+
+            // clear the LED status
+            digitalWrite(D7, 0);
+        }
+    }
+
+    delay(loopDelayInMilliseconds);
 }
